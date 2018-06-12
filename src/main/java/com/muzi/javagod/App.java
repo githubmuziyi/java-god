8b25524886b637afe9e022542e3af380f93f3ed7
muzi.he <muzi.habo@gmail.com>
2018-06-12 21:07:22 +0800

    jvm内存结构

---
+++ src/main/java/com/muzi/javagod/App.java
@@ -0,0 +1,13 @@
+package com.muzi.javagod;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
